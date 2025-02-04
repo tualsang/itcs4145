a0a555a97411dc186f0b7a1925b6839c01261ac2
Tuan S Sang <tsang3@gal-i9.charlotte.edu>
2025-02-04 12:45:16 -0500

    modified script and new result

--- myscript.sh
+++ myscript.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 #SBATCH --partition=Centaurus
-#SBATCH --time=00:01:00
+#SBATCH --time=01:00:00
+#SBATCH --mem=64G
 
 for size in 10 100 1000 10000 100000 1000000 10000000 100000000 1000000000; do
 	./mergesort $size
